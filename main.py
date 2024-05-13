97db5d62e138ccfa16852434b72db448840d00d2
Light2254 <chris1234567891269@gmail.com>
2024-05-13 22:04:48 +0800

    add GUI

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import qrcode
 import os
+import qrcode
 import shutil
+import tkinter as tk
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from tkinter import filedialog, ttk
 
-print("請輸入選擇的檔案")
-image_name=input("")
+root = tk.Tk()
+root.title('oxxo.studio')
+root.geometry('300x300')
 
-while image_name!="-1":
-    print("請輸入選擇的Gmail")
-    Gmail=input("")
-    match Gmail:
+text = tk.StringVar()   # 設定 text 為文字變數
+text.set('')            # 設定 text 的內容
+
+
+def Gmail():
+    a=box.get()
+    global UPLOAD_FOLDER
+    match a:
         case "1" :
-            UPLOAD_FOLDER = '1yhnJ6jdJM88pGXjq_SgbPLnIjoHXwLv2'
+            UPLOAD_FOLDER = '17RuRvT-e3n7Z850mtx1spSJo2Lw7nNt3'
             print(UPLOAD_FOLDER)
         case "2" :
             UPLOAD_FOLDER= '1o7cYPbNpPi53OCEdaED0iCkpz_1kjLVa'
@@ -22,19 +29,14 @@ while image_name!="-1":
         case _ :
             UPLOAD_FOLDER="ERROR"
             print(UPLOAD_FOLDER)
-
-    SCOPES = ['https://www.googleapis.com/auth/drive']
-    SERVICE_ACCOUNT_FILE = 'Key.json'  # 金鑰檔案
-
-    # 建立憑證物件
-    creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    service = build('drive', 'v3', credentials=creds)
-    
-    # 上傳檔的名字
-    filename = image_name +".jpg"    
+def choose():
+    print(UPLOAD_FOLDER)
+    filename = filedialog.askopenfilename()
+    image_name=filename[27:]
+    print(filename)
+    # 上傳檔的名字   
     media = MediaFileUpload(filename)
-    file = {'name': filename, 'parents': [UPLOAD_FOLDER]}
+    file = {'name': image_name, 'parents': [UPLOAD_FOLDER]}
 
     #上傳雲端
     print("正在上傳檔案...")
@@ -42,17 +44,44 @@ while image_name!="-1":
     print('雲端檔案ID：' + str(file_id['id']))
 
     #建立QRcode
+    print('正在建立QRcode')
     code="https://drive.google.com/file/d/"+str(file_id['id'])+"/view"
     img = qrcode.make(code)
-    img.save(image_name+".png")
+    img.save(image_name)
 
     #將圖檔移置指定路徑
-    f_src=os.path.join("./",image_name+".png")
+    f_src=os.path.join("./",image_name)
     if not os.path.exists("./qrcode/"):
         os.makedirs("./qrcode/")
-    f_dst=os.path.join("./qrcode/",image_name+".png")
+    f_dst=os.path.join("./qrcode/",image_name)
     shutil.move(f_src,f_dst)
+    print('QRcode建立完成')
+    print('請重新選擇檔案')
 
-    os.system("cls")
-    print("請輸入選擇的檔案")
-    image_name=input("")
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SERVICE_ACCOUNT_FILE = 'Key.json'  # 金鑰檔案
+
+# 建立憑證物件
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+service = build('drive', 'v3', credentials=creds)
+    
+box = ttk.Combobox(root,
+                width=20,
+                values=['1','2','3','4','5'])
+box.pack()
+# Button 設定 command 參數
+btn1 = tk.Button(root,
+            text='選擇 Gmail',
+            font=('Arial',20,'bold'),
+            command=Gmail)
+btn1.pack()
+
+btn2 = tk.Button(root,
+            text='選擇 檔案',
+            font=('Arial',20,'bold'),
+            command=choose)
+btn2.pack()
+
+
+root.mainloop()
